a7a8e4dcd44b34d40f4889b05027288f13ceb36c
algoridea <algoridea@gmail.com>
2011-10-12 21:54:10 +0900

    Initial commit

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e2d2b48654307d5270634443694e9ed0ce1f53567abca8581ad143564deb36b52b49d53527db60475464d0215bac9476f4f92158d5acc39d52f5fb49f95238fe'
